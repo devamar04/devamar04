9e729fa82964e660cb3cd2211926621aedd20fdc
Amar El Fatmi <Amar.ElFatmi1@es.nestle.com>
2022-09-02 15:16:41 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [error, setError] = useState();
+
   useEffect(() => {
     console.log("parent", window.parent);
-    window.parent.postMessage("im the man", "*");
+    try {
+      window.parent.postMessage("im the man", "*");
+    } catch (error) {
+      setError(error);
+    }
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>TEST</h1>
+        <h1>{JSON.stringify(error)}</h1>
       </header>
     </div>
   );
